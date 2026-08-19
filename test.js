88825ae31eb2337da3c74fa8120bb7d58fbaf8e6
Khalif-Tahir <aelhangari@gmail.com>
2026-08-20 00:28:03 +0200

    initializing

---
+++ test.js
@@ -0,0 +1,8 @@
+// script.js
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("clickMe");
+
+  btn.addEventListener("click", () => {
+    alert("Hello! You clicked the button.");
+  });
+});
